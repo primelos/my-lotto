9e13a6e3a10a4cd9041735551e1e7f42bd28e923
Ale <alearanibar@gmail.com>
2021-01-11 14:01:11 -0800

    added power and mega components and connected it with react router dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import Lotto from './components/lotto'
-
+import { Route, Switch } from 'react-router-dom'
+import HomePage from './pages/homepage'
+import Mega from './components/mega'
+import Power from './components/power'
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Lets Win Some Money!!!</h1>
-      <Lotto />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/lotto" component={Lotto} />
+        <Route exact path="/mega" component={Mega} />
+        <Route exact path="/power" component={Power} />
+      </Switch>
     </div>
   );
 }
